c575e6a6fd2c7ca601f13e4fea51ae9cf20895a2
Pavel Lazureykis <lazureykis@gmail.com>
2015-03-17 15:12:21 +0300

    UPD refactored test

--- dotenv_test.go
+++ dotenv_test.go
@@ -10,33 +10,23 @@ func printEnv() {
 	fmt.Println(os.Environ())
 }
 
+func assertEnvValue(t *testing.T, name, want string) {
+	got := os.Getenv(name)
+	if got != want {
+		t.Errorf("Want %v but got %v\n", want, got)
+	}
+}
+
 func TestGoPlainWithTrimming(t *testing.T) {
 	err := GoWithPath("fixtures/plain.env")
 	if err != nil {
 		t.Error(err)
 	}
 
-	if os.Getenv("PLAIN") != "true" {
-		t.Error("Wrong value PLAIN, should be 'true'")
-	}
-
-	if os.Getenv("OPTION_A") != "1" {
-		t.Error("Wrong value OPTION_A, should be '1'")
-	}
-
-	if os.Getenv("OPTION_B") != "2" {
-		t.Error("Wrong value OPTION_B, should be '2'")
-	}
-
-	if os.Getenv("OPTION_C") != "3" {
-		t.Error("Wrong value OPTION_C, should be '3'")
-	}
-
-	if os.Getenv("OPTION_D") != "4" {
-		t.Error("Wrong value OPTION_D, should be '4'")
-	}
-
-	if os.Getenv("OPTION_E") != "5" {
-		t.Error("Wrong value OPTION_E, should be '5'")
-	}
+	assertEnvValue(t, "PLAIN", "true")
+	assertEnvValue(t, "OPTION_A", "1")
+	assertEnvValue(t, "OPTION_B", "2")
+	assertEnvValue(t, "OPTION_C", "3")
+	assertEnvValue(t, "OPTION_D", "4")
+	assertEnvValue(t, "OPTION_E", "5")
 }
